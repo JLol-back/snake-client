400f18d69a98aff6d9dbd77bc3e3cf5aebe0d2e0
Jake Lollback <jalollback@gmail.com>
2023-08-09 21:15:15 -0500

    Add constants for IP and PORT

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
 // establishes a connection with the game server
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
+    host: IP,
+    port: PORT
   });
   // Log message on successful connection to server
   conn.on("connect", () => {
